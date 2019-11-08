94b9a80b065928441621e9860f9705b52b7fee02
Steven Laff <steve.laff@gmail.com>
2019-11-08 03:38:24 +0000

    Automatically backed up by Learn

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -1,7 +1,8 @@
 class CreateActors < ActiveRecord::Migration[5.1]
   def change
     create_table :actors do |a|
-      a.string :name
+      a.string :first_name
+      a.string :last_name
     end
   end
 end
